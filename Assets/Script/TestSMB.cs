ef73eef04be9eb121ade8d5f5578a5365244040b
fauder_office <canik.burak@gmail.com>
2023-01-24 17:38:30 +0300

    Serialization Test 3: Dictionary

--- Assets/Script/TestSMB.cs
+++ Assets/Script/TestSMB.cs
@@ -1,22 +1,30 @@
 /* Created by and for usage of FF Studios (2021). */
 
 using UnityEngine;
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 
 public class TestSMB : SerializedMonoBehaviour
 {
 	[ SerializeField ] ISomeInterface[] interface_array;
     [ SerializeField ] TestSSO testSSO;
+    [ SerializeField ] Dictionary< int, int > dictionary;
 
 	void Start()
 	{
 		foreach( var interf in interface_array )
 			interf.Execute();
             
-        FFStudio.FFLogger.Log( name + ": TestSMB will now invoke Execute() on elements of TestSSO's interface_array.", this );
+        Debug.Log( name + ": TestSMB will now invoke Execute() on elements of TestSSO's interface_array.", this );
         
 		foreach( var interf in testSSO.interface_array )
 			interf.Execute();
+
+		Debug.Log( "Will log the dictionary now:" );
+		foreach( var kvp in dictionary )
+        {
+			Debug.Log( kvp );
+		}
 	}
 }
 
